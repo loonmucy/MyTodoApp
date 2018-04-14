500d48fe83595461ae30d55166235c7c348deab0
loonmucy <1540259636@qq.com>
2018-04-14 19:35:12 +0800

    todolist

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+      {{ todo.text }}
+      <button @click="$emit('remove',todo.id)">
+          ×
+      </button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+      todo: {
+          type: Object,
+          required: true
+      }
+  }
+}
+</script>
+
